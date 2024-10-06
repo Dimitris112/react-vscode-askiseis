f52847539f0b05131a3a04813ff38c65ed2c3ab2
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-06 15:54:31 +0300

    Render component inside loop - add Fruit component

--- Prwto_App/src/components/Fruits.jsx
+++ Prwto_App/src/components/Fruits.jsx
@@ -1,3 +1,4 @@
+import Fruit from "./Fruit";
 export default function Fruits() {
   // const fruits = ["Apple", "Banana", "Orange", "Mango"];
   const fruits = [
@@ -10,9 +11,12 @@ export default function Fruits() {
     <div>
       <ul>
         {fruits.map((fruit) => (
-          <li key={fruit.name}>
-            {fruit.name} ${fruit.price} {fruit.emoji}
-          </li>
+          <Fruit
+            key={fruit.name}
+            name={fruit.name}
+            price={fruit.price}
+            emoji={fruit.emoji}
+          />
         ))}
       </ul>
     </div>
