0acc9c17dc4cac290b320243361d7dab72046905
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-06 16:25:32 +0300

    Tenary operator

--- Prwto_App/src/components/ConditionalComponent.jsx
+++ Prwto_App/src/components/ConditionalComponent.jsx
@@ -2,18 +2,6 @@ import Welcome from "./Welcome";
 import Code from "./Code";
 
 export default function ConditionalComponent() {
-  //   let messageOne = <h1>This is message 1</h1>;
-  //   let messageTwo = <h1>This is message 2</h1>;
-
-  let message;
   const display = true;
-
-  if (display) {
-    message = <h1>This is message 1</h1>;
-  } else {
-    message = <h1>This is message 2</h1>;
-  }
-  return message;
+  return display ? <Welcome /> : <Code />;
 }
-
-// let message = () => display ? messageOne : messageTwo;
