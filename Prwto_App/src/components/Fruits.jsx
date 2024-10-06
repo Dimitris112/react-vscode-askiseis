89c94fd58bf68d283d6fb7a835fbefec72a983c8
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-06 13:14:52 +0300

    Render array of objects

--- Prwto_App/src/components/Fruits.jsx
+++ Prwto_App/src/components/Fruits.jsx
@@ -1,10 +1,18 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Banana", "Orange", "Mango"];
+  // const fruits = ["Apple", "Banana", "Orange", "Mango"];
+  const fruits = [
+    { name: "Apple", price: 10, emoji: "🍎" },
+    { name: "Banana", price: 5, emoji: "🍌" },
+    { name: "Orange", price: 15, emoji: "🍊" },
+    { name: "Mango", price: 20, emoji: "🥭" },
+  ];
   return (
     <div>
       <ul>
         {fruits.map((fruit) => (
-          <li key={fruit}>{fruit}</li>
+          <li key={fruit.name}>
+            {fruit.name} ${fruit.price} {fruit.emoji}
+          </li>
         ))}
       </ul>
     </div>
